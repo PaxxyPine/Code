2f3c1d966ff248dc14af33a99091d06612fe2509
Matt Enwald <matt.enwald@gmail.com>
2022-07-03 14:36:39 -0500

    2 itmes in belt

--- dungeonexplorer/dungeon-server/internal/gameengine/belts.go
+++ dungeonexplorer/dungeon-server/internal/gameengine/belts.go
@@ -27,3 +27,51 @@ func (helm *ClothBelt) Class() int64 {
 func (helm *ClothBelt) Type() Code.ArmorType {
 	return Code.ArmorType_ArmorType_BELT
 }
+
+type IronBelt struct {
+	id int64
+}
+
+func (belt *IronBelt) Name() string {
+	return "Iron Belt"
+}
+
+func (belt *IronBelt) Description() string {
+	return "A rich iron belt"
+}
+
+func (belt *IronBelt) GoldValue() int64 {
+	return 20
+}
+
+func (belt *IronBelt) Class() int64 {
+	return 2
+}
+
+func (belt *IronBelt) Type() Code.ArmorType {
+	return Code.ArmorType_ArmorType_BELT
+}
+
+type WitchBelt struct {
+	id int64
+}
+
+func (belt *WitchBelt) Name() string {
+	return "Witch Belt"
+}
+
+func (belt *WitchBelt) Description() string {
+	return "The witchs old master belt"
+}
+
+func (belt *WitchBelt) GoldValue() int64 {
+	return 30
+}
+
+func (belt *WitchBelt) Class() int64 {
+	return 3
+}
+
+func (belt *WitchBelt) Type() Code.ArmorType {
+	return Code.ArmorType_ArmorType_BELT
+}
