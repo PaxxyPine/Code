baf696add3ac33f59bf7c00216d83c5f6fd8f165
Matt Enwald <matt.enwald@gmail.com>
2022-07-04 14:24:45 -0500

    items hat

--- dungeonexplorer/dungeon-server/internal/gameengine/boots.go
+++ dungeonexplorer/dungeon-server/internal/gameengine/boots.go
@@ -31,3 +31,59 @@ func (boot *ClothBoots) Class() int64 {
 func (boot *ClothBoots) Type() Code.ArmorType {
 	return Code.ArmorType_ArmorType_BOOTS
 }
+
+type Runners struct {
+	id int64
+}
+
+func (boot *Runners) Id() int64 {
+	return boot.id
+}
+
+func (boot *Runners) Name() string {
+	return "Runners"
+}
+
+func (boot *Runners) Description() string {
+	return "Got the running drip"
+}
+
+func (boot *Runners) GoldValue() int64 {
+	return 3
+}
+
+func (boot *Runners) Class() int64 {
+	return 1
+}
+
+func (boot *Runners) Type() Code.ArmorType {
+	return Code.ArmorType_ArmorType_BOOTS
+}
+
+type IronBoots struct {
+	id int64
+}
+
+func (boot *IronBoots) Id() int64 {
+	return boot.id
+}
+
+func (boot *IronBoots) Name() string {
+	return "IronBoots"
+}
+
+func (boot *IronBoots) Description() string {
+	return "boots made from hard cold steel"
+}
+
+func (boot *IronBoots) GoldValue() int64 {
+	return 3
+}
+
+func (boot *IronBoots) Class() int64 {
+	return 1
+}
+
+func (boot *IronBoots) Type() Code.ArmorType {
+	return Code.ArmorType_ArmorType_BOOTS
+}
--- dungeonexplorer/dungeon-server/internal/gameengine/helmets.go
+++ dungeonexplorer/dungeon-server/internal/gameengine/helmets.go
@@ -21,13 +21,97 @@ func (helm *ClothHat) Description() string {
 }
 
 func (helm *ClothHat) GoldValue() int64 {
-	return 3
+	return 30
 }
 
 func (helm *ClothHat) Class() int64 {
-	return 1
+	return 2
 }
 
 func (helm *ClothHat) Type() Code.ArmorType {
+	return Code.ArmorType_ArmorType_HELM
+}
+
+type VikingHat struct {
+	id int64
+}
+
+func (helm *VikingHat) Id() int64 {
+	return helm.id
+}
+
+func (helm *VikingHat) Name() string {
+	return "Viking Hat"
+}
+
+func (helm *VikingHat) Description() string {
+	return "lost item from the past"
+}
+
+func (helm *VikingHat) GoldValue() int64 {
+	return 50
+}
+
+func (helm *VikingHat) Class() int64 {
+	return 6
+}
+
+func (helm *VikingHat) Type() Code.ArmorType {
+	return Code.ArmorType_ArmorType_HELM
+}
+
+type UmbrellaHat struct {
+	id int64
+}
+
+func (helm *UmbrellaHat) Id() int64 {
+	return helm.id
+}
+
+func (helm *UmbrellaHat) Name() string {
+	return "Umbrella Hat "
+}
+
+func (helm *UmbrellaHat) Description() string {
+	return "keep the drip away also keeping your drip"
+}
+
+func (helm *UmbrellaHat) GoldValue() int64 {
+	return 69
+}
+
+func (helm *UmbrellaHat) Class() int64 {
+	return 3
+}
+
+func (helm *UmbrellaHat) Type() Code.ArmorType {
+	return Code.ArmorType_ArmorType_HELM
+}
+
+type TopHat struct {
+	id int64
+}
+
+func (helm *TopHat) Id() int64 {
+	return helm.id
+}
+
+func (helm *TopHat) Name() string {
+	return "Top Hat"
+}
+
+func (helm *TopHat) Description() string {
+	return "FELXXX"
+}
+
+func (helm *TopHat) GoldValue() int64 {
+	return 300
+}
+
+func (helm *TopHat) Class() int64 {
+	return 0
+}
+
+func (helm *TopHat) Type() Code.ArmorType {
 	return Code.ArmorType_ArmorType_HELM
 }
