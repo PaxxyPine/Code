af52e8f228bd92bb1b559beffe6937eec420eaf9
Matt Enwald <matt.enwald@gmail.com>
2022-07-04 22:02:28 -0500

    rings and fin items

--- dungeonexplorer/dungeon-server/internal/gameengine/chests.go
+++ dungeonexplorer/dungeon-server/internal/gameengine/chests.go
@@ -25,36 +25,64 @@ func (chest *ClothChest) GoldValue() int64 {
 }
 
 func (chest *ClothChest) Class() int64 {
-	return 2
+	return
 }
 
 func (chest *ClothChest) Type() Code.ArmorType {
 	return Code.ArmorType_ArmorType_CHEST
 }
-type Chainmailchestplate struct {
+
+type BlackHawkPlate struct {
 	id int64
 }
 
-func (chest *Chainmailchestplate) Id() int64 {
+func (chest *BlackHawkPlate) Id() int64 {
 	return chest.id
 }
 
-func (chest *Chainmailchestplate) Name() string {
-	return "Chainmail chestplate"
+func (chest *BlackHawkPlate) Name() string {
+	return "Black Hawk chestplate"
 }
 
-func (chest *Chainmailchestplate-) Description() string {
-	return "you could make this out of can caps!"
+func (chest *BlackHawkPlate) Description() string {
+	return "pure evil held in a plate that belongs on your chest"
 }
 
-func (chest *Chainmailchestplate) GoldValue() int64 {
-	return 40
+func (chest *BlackHawkPlate) GoldValue() int64 {
+	return 60
 }
 
-func (chest *Chainmailchestplate) Class() int64 {
-	return 4
+func (chest *BlackHawkPlate) Class() int64 {
+	return 6
 }
 
-func (chest *Chainmailchestplate) Type() Code.ArmorType {
+func (chest *BlackHawkPlate) Type() Code.ArmorType {
+	return Code.ArmorType_ArmorType_CHEST
+}
+
+type InronChestPlate struct {
+	id int64
+}
+
+func (chest *InronChestPlate) Id() int64 {
+	return chest.id
+}
+
+func (chest *InronChestPlate) Name() string {
+	return "Inron chestplate"
+}
+
+func (chest *InronChestPlate) Description() string {
+	return "wow 1000000 pounds is pretty light!!"
+}
+func (chest *InronChestPlate) GoldValue() int64 {
+	return 50
+}
+
+func (chest *InronChestPlate) Class() int64 {
+	return 5
+}
+
+func (chest *InronChestPlate) Type() Code.ArmorType {
 	return Code.ArmorType_ArmorType_CHEST
 }
--- dungeonexplorer/dungeon-server/internal/gameengine/legs.go
+++ dungeonexplorer/dungeon-server/internal/gameengine/legs.go
@@ -31,3 +31,59 @@ func (leg *ClothPants) Class() int64 {
 func (leg *ClothPants) Type() Code.ArmorType {
 	return Code.ArmorType_ArmorType_LEGS
 }
+
+type ChainMailPants struct {
+	id int64
+}
+
+func (leg *ChainMailPants) Id() int64 {
+	return leg.id
+}
+
+func (leg *ChainMailPants) Name() string {
+	return "Cloth Pants"
+}
+
+func (leg *ChainMailPants) Description() string {
+	return "you can also make these with bottle caps!"
+}
+
+func (leg *ChainMailPants) GoldValue() int64 {
+	return 35
+}
+
+func (leg *ChainMailPants) Class() int64 {
+	return 3
+}
+
+func (leg *ChainMailPants) Type() Code.ArmorType {
+	return Code.ArmorType_ArmorType_LEGS
+}
+
+type IronPants struct {
+	id int64
+}
+
+func (leg *IronPants) Id() int64 {
+	return leg.id
+}
+
+func (leg *IronPants) Name() string {
+	return "Iron Pants"
+}
+
+func (leg *IronPants) Description() string {
+	return "you can also make these with bottle caps!"
+}
+
+func (leg *IronPants) GoldValue() int64 {
+	return 45
+}
+
+func (leg *IronPants) Class() int64 {
+	return 4
+}
+
+func (leg *IronPants) Type() Code.ArmorType {
+	return Code.ArmorType_ArmorType_LEGS
+}
