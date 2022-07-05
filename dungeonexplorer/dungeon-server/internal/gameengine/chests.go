5067e88a3c64202a8664f7511c516b930b68414d
Matt Enwald <matt.enwald@gmail.com>
2022-07-04 21:33:36 -0500

    more items chest (if ur reading fix chest problem there)

--- dungeonexplorer/dungeon-server/internal/gameengine/chests.go
+++ dungeonexplorer/dungeon-server/internal/gameengine/chests.go
@@ -21,13 +21,40 @@ func (chest *ClothChest) Description() string {
 }
 
 func (chest *ClothChest) GoldValue() int64 {
-	return 3
+	return 30
 }
 
 func (chest *ClothChest) Class() int64 {
-	return 1
+	return 2
 }
 
 func (chest *ClothChest) Type() Code.ArmorType {
+	return Code.ArmorType_ArmorType_CHEST
+}
+type Chainmailchestplate struct {
+	id int64
+}
+
+func (chest *Chainmailchestplate) Id() int64 {
+	return chest.id
+}
+
+func (chest *Chainmailchestplate) Name() string {
+	return "Chainmail chestplate"
+}
+
+func (chest *Chainmailchestplate-) Description() string {
+	return "you could make this out of can caps!"
+}
+
+func (chest *Chainmailchestplate) GoldValue() int64 {
+	return 40
+}
+
+func (chest *Chainmailchestplate) Class() int64 {
+	return 4
+}
+
+func (chest *Chainmailchestplate) Type() Code.ArmorType {
 	return Code.ArmorType_ArmorType_CHEST
 }
